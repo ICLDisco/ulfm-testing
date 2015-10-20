c87b3b02bc444cf5ad91bf49fbaba8c3d1deb78f
Aurelien Bouteiller <bouteill@icl.utk.edu>
2015-10-20 15:57:48 -0400

    Upgrade the tutorial simplest examples with errhandlers

---
+++ tutorial/ex0.0.failure.c
@@ -0,0 +1,33 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
+/*
+ * Copyright (c) 2013-2014 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#include <mpi.h>
+#include <mpi-ext.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main(int argc, char *argv[])
+{
+    int rank, size;
+
+    MPI_Init(NULL, NULL);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    if( rank == (size-1) ) raise(SIGKILL);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    printf("Rank %d / %d\n", rank, size);
+
+    MPI_Finalize();
+}
