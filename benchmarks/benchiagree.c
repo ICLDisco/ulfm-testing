de1e855233f5477abc4b1650860bb48fbbda3576
Aurelien Bouteiller <bouteill@icl.utk.edu>
2015-12-01 16:41:58 -0500

    Change the work in iagree so it doesn't get compiled out

--- benchmarks/benchiagree.c
+++ benchmarks/benchiagree.c
@@ -4,9 +4,9 @@
  *                         reserved.
  *
  * $COPYRIGHT$
- * 
+ *
  * Additional copyrights may follow
- * 
+ *
  * $HEADER$
  */
 #include <mpi.h>
@@ -15,25 +15,28 @@
 
 #define MAX_COMPUTE_LOOP 2048
 
-int main(void) { 
-    double ts, tp, tw, te, vv;
-    int do_compute, i;
+volatile double vv;
+
+int main(void) {
+    double ts, tp, tw, te, v;
+    int do_compute, i, j;
     MPI_Request req;
     int rank, flag = 0;
 
     MPI_Init( NULL, NULL );
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     flag = 1<<(rank%sizeof(int));
-    
+
     for( do_compute = 1; do_compute < MAX_COMPUTE_LOOP; do_compute *= 2 ) {
         ts = MPI_Wtime();
         MPIX_Comm_iagree( MPI_COMM_WORLD, &flag, &req );
         tp = MPI_Wtime();
 
-        for( i = 0; i < do_compute; i++ ) {
-            vv++;
+        v = 1.0;
+        for( i = 0; i < do_compute; i++ ) for( j = 0; j < do_compute; j++ ) {
+            v += 0.5 * v;
         }
-        
+        vv=v;
         tw = MPI_Wtime();
         MPI_Wait( &req, MPI_STATUS_IGNORE );
         te = MPI_Wtime();
