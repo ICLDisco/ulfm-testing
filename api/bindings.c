4666f02b8ddd4bdd5dc28e5ee93ce1a5af75f63d
Aurelien Bouteiller <bouteill@icl.utk.edu>
2016-01-25 15:13:29 -0500

    Revamping the conformance test to use the MPIX versions of the variables

--- api/bindings.c
+++ api/bindings.c
@@ -18,8 +18,6 @@
 #include <stdlib.h>
 #include <signal.h>
 
-int rank;
-
 /* verify if the interface is complete and provided.
  * As per MPI draft spec, these interface may not be
  * functional under the duress of failures.
@@ -28,40 +26,43 @@ int main(void) {
     MPI_Comm shrink;
     MPI_Group fgroup;
     MPI_Request req;
+    int rank;
     int flag = 0;
     int *ftsupport = NULL;
+    int err;
     char errs[MPI_MAX_ERROR_STRING]; int errsl;
 
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    MPI_Comm_get_attr(MPI_COMM_WORLD, MPI_FT, &ftsupport, &flag);
+    MPI_Comm_get_attr(MPI_COMM_WORLD, MPIX_FT, &ftsupport, &flag);
     if( flag ) {
-        printf("AVAILABLE: MPI_FT Attribute key (SET %d: ULFM%s SUPPORTED)\n", *ftsupport, (1==*ftsupport)? "": " NOT");
+        printf("COMPLIANT:\tULFM CAN%s tolerate failures.\tAttribute value for key MPIX_FT is SET to %d.\n", (1==*ftsupport)? "": "NOT", *ftsupport);
     }
     else {
-        printf("AVAILABLE: MPI_FT Attribute key (NOT SET: ULFM NOT SUPPORTED)\n");
+        printf("COMPLIANT:\tULFM CANNOT tolerate failures.\tAttribute value for key MPIX_FT is NOT SET\n");
     }
     MPI_Error_string(MPIX_ERR_PROC_FAILED, errs, &errsl);
-    printf("AVAILABLE: %s\n", errs);
+    printf("COMPLIANT:\tError class exists:\t\t%s\n", errs);
     MPI_Error_string(MPIX_ERR_PROC_FAILED_PENDING, errs, &errsl);
-    printf("AVAILABLE: %s\n", errs);
+    printf("COMPLIANT:\tError class exists:\t\t%s\n", errs);
     MPI_Error_string(MPIX_ERR_REVOKED, errs, &errsl);
-    printf("AVAILABLE: %s\n", errs);
+    printf("COMPLIANT:\tError class exists:\t\t%s\n", errs);
 
-    MPIX_Comm_failure_ack(MPI_COMM_WORLD);
-    printf("AVAILABLE: %s\n", "MPIX_Comm_failure_ack");
-    MPIX_Comm_failure_get_acked(MPI_COMM_WORLD, &fgroup);
-    printf("AVAILABLE: %s\n", "MPIX_Comm_failure_get_acked");
-    MPIX_Comm_agree(MPI_COMM_WORLD, &flag);
-    printf("AVAILABLE: %s\n", "MPIX_Comm_agree");
-    MPIX_Comm_iagree(MPI_COMM_WORLD, &flag, &req);
-    MPI_Wait(&req, MPI_STATUS_IGNORE);
-    printf("AVAILABLE: %s\n", "MPIX_Comm_iagree");
-    MPIX_Comm_shrink(MPI_COMM_WORLD, &shrink);
-    printf("AVAILABLE: %s\n", "MPIX_Comm_shrink");
-    MPIX_Comm_revoke(MPI_COMM_WORLD);
-    printf("AVAILABLE: %s\n", "MPIX_Comm_revoke");
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
+
+#define TEST_MACRO( TEST_FN, TEST_FN_PARAMS ) do { \
+    err = TEST_FN TEST_FN_PARAMS ; \
+    MPI_Error_string(err, errs, &errsl); \
+    if( 0 == rank || MPI_SUCCESS != err ) printf("%sCOMPLIANT:\t%-32s%s\n", (MPI_SUCCESS==err)? "": "NOT ", #TEST_FN, errs); \
+} while(0)
+
+    TEST_MACRO( MPIX_Comm_failure_ack, (MPI_COMM_WORLD) );
+    TEST_MACRO( MPIX_Comm_failure_get_acked, (MPI_COMM_WORLD, &fgroup) );
+    TEST_MACRO( MPIX_Comm_agree, (MPI_COMM_WORLD, &flag) );
+    TEST_MACRO( MPIX_Comm_iagree, (MPI_COMM_WORLD, &flag, &req); MPI_Wait(&req, MPI_STATUS_IGNORE) );
+    TEST_MACRO( MPIX_Comm_shrink, (MPI_COMM_WORLD, &shrink) );
+    TEST_MACRO( MPIX_Comm_revoke, (MPI_COMM_WORLD) );
 
     MPI_Finalize();
     return 0;
