ef64b60cfc41f5bcea6a1b8ad97e26dcac650dd1
Aurelien Bouteiller <bouteill@icl.utk.edu>
2016-03-09 15:17:18 -0500

    Fix some warnings in the tutorial examples.

--- tutorial/ex1.2.safe_comm_grid2d.c
+++ tutorial/ex1.2.safe_comm_grid2d.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
- * Copyright (c) 2013-2015 The University of Tennessee and The University
+ * Copyright (c) 2013-2016 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * $COPYRIGHT$
@@ -12,6 +12,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <signal.h>
 #include <math.h>
 #include <mpi.h>
