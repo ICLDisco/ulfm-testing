176e80a2b487e31d4cc31eaa2261883010a0f15e
Aurelien Bouteiller <bouteill@icl.utk.edu>
2016-04-04 17:42:07 -0400

    Modification of benchdetect

--- benchmarks/benchdetect_recvany.c
+++ benchmarks/benchdetect_recvany.c
@@ -12,6 +12,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
 #include <string.h>
 #include <signal.h>
 #include <math.h>
@@ -22,19 +24,62 @@ int rank, verbose=0; /* makes this global (for printfs) */
 
 int main( int argc, char* argv[] ) {
     MPI_Comm fcomm, scomm; /* a comm to inject a failure, and a safe comm */
-    int np, victim; /* the victim rank */
+    MPI_Group fgrp; /* a group to collect failed ranks */
+    int np, nf, mf, victim, notify=0; /* the number of victims ranks */
     int dummy = 1; /* some buffer for Recv */
-    int rc; /* error code from MPI functions */
+    int i, rc; /* error code from MPI functions */
     char estr[MPI_MAX_ERROR_STRING]=""; int strl; /* error messages */
     double start, tff, twf; /* timings */
 
     MPI_Init(NULL, NULL);
     MPI_Comm_size( MPI_COMM_WORLD, &np );
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    if( !strcmp( argv[argc-1], "-v" ) ) verbose=1;
 
-    /* The victim is always the last process (for simplicity) */
-    victim = (rank == np-1)? 1 : 0;
+    int* faults = (int*)calloc(np, sizeof(int));
+
+    while(1) {
+        static struct option long_options[] = {
+            { "verbose",      0, 0, 'v' },
+            { "faults",       1, 0, 'f' },
+            { "multifaults",  1, 0, 'm' },
+            { "injectreport", 0, 0, 'i' },
+            { NULL,           0, 0, 0   }
+        };
+
+        int c = getopt_long(argc, argv, "vif:m:", long_options, NULL);
+        if (c == -1)
+            break;
+
+        switch(c) {
+        case 'v':
+            verbose = 1;
+            break;
+        case 'i':
+            notify = 1;
+            break;
+        case 'f':
+            faults[ atoi(optarg) ] = 1;
+            break;
+        case 'm':
+            mf=atoi(optarg);
+            break;
+        }
+    }
+
+    srand(1);
+    for(i = 0; i < mf; i++) {
+        do {
+            rc = rand();
+            rc = rc % np;
+            if(!faults[rc]) {
+                faults[rc] = 1;
+                break;
+            }
+        } while(1);
+    }
+
+    /* Am I a victim? */
+    victim = faults[rank]? 1 : 0;
 
     /* Now, we need a communicator that still works after the failure:
      *  this split creates a communicator that excludes the victim;
@@ -44,33 +89,33 @@ int main( int argc, char* argv[] ) {
 
     /* Let's work on a copy of MPI_COMM_WORLD, good practice anyway. */
     MPI_Comm_dup( MPI_COMM_WORLD, &fcomm );
-    /* synchronize */
-    MPI_Barrier( MPI_COMM_WORLD);
-
-    /* Do a send-recv ring with ANY_SOURCE, and time it */
-    start=MPI_Wtime();
-    MPI_Sendrecv(&rank,  1, MPI_INT, (rank+1)%np, 0,
-                 &dummy, 1, MPI_INT, MPI_ANY_SOURCE, 0,
-                 fcomm, MPI_STATUS_IGNORE);
-    tff=MPI_Wtime()-start;
-
-    MPI_Barrier( fcomm );
-    if( 0 == rank ) printf("#####################################################\n");
-
     /* We set an errhandler on fcomm, so that a failure is not fatal anymore.
      * Note: the errhandler on scomm and MPI_COMM_WORLD are still the
      *   default: if an operation involves a failed process on these
      *   communicators, the program still aborts! */
     MPI_Comm_set_errhandler( fcomm, MPI_ERRORS_RETURN );
 
+    /* synchronize */
+    MPI_Barrier( MPI_COMM_WORLD);
+    /* Do a send-recv ring with ANY_SOURCE, and time it */
+    start=MPI_Wtime();
+    MPI_Sendrecv(&rank,  1, MPI_INT, (rank+1)%np, 0,
+                 &dummy, 1, MPI_INT, MPI_ANY_SOURCE, 0,
+                 fcomm, MPI_STATUS_IGNORE);
+    tff=MPI_Wtime()-start;
+    /* synchronize */
+    MPIX_Comm_agree(fcomm, &rc);
+    if( 0 == rank ) printf("#####################################################\n");
+
+do {
     /* Victim suicides */
     if( victim ) {
-        printf( "Rank %04d: committing suicide\n", rank );
-        raise( SIGKILL );
+        printf( "Rank %04d: committing suicide at date %.9g\n", rank, MPI_Wtime() );
+        OMPI_Comm_failure_inject(fcomm, notify);
     }
 
     /* Do a recv from any_source : it will raise an exception: somebody is dead */
-    if(verbose) printf( "Rank %04d: entering Recv(any)\n", rank );
+    if(verbose) printf( "Rank %04d: entering Recv(any) at date %.9g\n", rank, MPI_Wtime() );
     start=MPI_Wtime();
     /* do not post the send so that the recv remains locked until failure is
      * raised */
@@ -78,12 +123,17 @@ int main( int argc, char* argv[] ) {
     twf=MPI_Wtime()-start;
     if(verbose) {
         MPI_Error_string( rc, estr, &strl );
-        printf( "Rank %04d: Recv1 completed (rc=%s) duration %g (s)\n", rank, estr, twf );
+        printf( "Rank %04d: Recv1 completed (rc=%s) duration %g (s) at date %.9g\n", rank, estr, twf, MPI_Wtime() );
     }
     /* From the code flow, we know that the failure happened before
      * the send, by the semantic of the Recv, it is thereby
      * impossible for any rank to complete succesfully! */
     if( rc != MPI_ERR_PROC_FAILED ) MPI_Abort( MPI_COMM_WORLD, rc );
+
+    MPIX_Comm_failure_ack(fcomm);
+    MPIX_Comm_failure_get_acked(fcomm, &fgrp);
+    MPI_Group_size(fgrp, &nf);
+} while( nf != mf );
 
     print_timings( scomm, tff, twf );
 
