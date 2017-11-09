a571c9f2e12bb0b1af9942603bccf3128250e6e4
Aurelien Bouteiller <bouteill@icl.utk.edu>
2017-11-09 13:43:51 -0500

    Make sure failures does not get injected before the errhandler is set

--- benchmarks/benchdetect_barrier.c
+++ benchmarks/benchdetect_barrier.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
- * Copyright (c) 2013-2014 The University of Tennessee and The University
+ * Copyright (c) 2013-2017 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * $COPYRIGHT$
@@ -15,17 +15,18 @@
 #include <signal.h>
 #include <math.h>
 #include <mpi.h>
+#include <assert.h>
 
 void print_timings( MPI_Comm scomm, double tff, double twf );
 int rank, verbose=0; /* makes this global (for printfs) */
 
-int main( int argc, char* argv[] ) { 
+int main( int argc, char* argv[] ) {
     MPI_Comm fcomm, scomm; /* a comm to inject a failure, and a safe comm */
     int np, victim; /* the victim rank */
     int rc; /* error code from MPI functions */
     char estr[MPI_MAX_ERROR_STRING]=""; int strl; /* error messages */
     double start, tff, twf; /* timings */
-    
+
     MPI_Init( &argc, &argv );
     MPI_Comm_size( MPI_COMM_WORLD, &np );
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
@@ -48,35 +49,36 @@ int main( int argc, char* argv[] ) {
     MPI_Barrier( fcomm );
     tff=MPI_Wtime()-start;
 
-    /* We set an errhandler on fcomm, so that a failure is not fatal anymore. 
+    /* We set an errhandler on fcomm, so that a failure is not fatal anymore.
      * Note: the errhandler on scomm and MPI_COMM_WORLD are still the
      *   default: if an operation involves a failed process on these
      *   communicators, the program still aborts! */
     MPI_Comm_set_errhandler( fcomm, MPI_ERRORS_RETURN );
 
+    MPI_Barrier(fcomm);
     /* Victim suicides */
     if( victim ) {
         printf( "Rank %04d: committing suicide\n", rank );
         raise( SIGKILL );
     }
-    
+
     /* Do another barrier: it will raise an exception: somebody is dead */
     if(verbose) printf( "Rank %04d: entering Barrier\n", rank );
     start=MPI_Wtime();
     rc = MPI_Barrier( fcomm );
     twf=MPI_Wtime()-start;
-    if(verbose) { 
+    if(verbose) {
         MPI_Error_string( rc, estr, &strl );
         printf( "Rank %04d: Barrier1 completed (rc=%s) duration %g (s)\n", rank, estr, twf );
     }
     /* From the code flow, we know that the failure happened before
-     * the barrier, by the semantic of the barrier it is thereby 
+     * the barrier, by the semantic of the barrier it is thereby
      * impossible for any rank to complete succesfully! */
     if( rc != MPI_ERR_PROC_FAILED ) MPI_Abort( MPI_COMM_WORLD, rc );
 
     print_timings( scomm, tff, twf );
 
-    /* Even though fcomm contains failed processes, we free it: 
+    /* Even though fcomm contains failed processes, we free it:
      *  this gives an opportunity for MPI to reclaim the resources. */
     MPI_Comm_free( &fcomm );
     MPI_Comm_free( &scomm );
@@ -86,24 +88,24 @@ int main( int argc, char* argv[] ) {
     return EXIT_SUCCESS;
 }
 
-void print_timings( MPI_Comm scomm, 
-                    double tff, 
+void print_timings( MPI_Comm scomm,
+                    double tff,
                     double twf ) {
     /* Storage for min and max times */
     double mtff, Mtff, mtwf, Mtwf;
 
-    /* Note: operation on scomm should not raise an error, only procs 
-     * not appearing in scomm are dead. 
+    /* Note: operation on scomm should not raise an error, only procs
+     * not appearing in scomm are dead.
      * No need to check rc: on scomm, error aborts */
     MPI_Reduce( &tff, &mtff, 1, MPI_DOUBLE, MPI_MIN, 0, scomm );
     MPI_Reduce( &tff, &Mtff, 1, MPI_DOUBLE, MPI_MAX, 0, scomm );
     MPI_Reduce( &twf, &mtwf, 1, MPI_DOUBLE, MPI_MIN, 0, scomm );
     MPI_Reduce( &twf, &Mtwf, 1, MPI_DOUBLE, MPI_MAX, 0, scomm );
 
-    if( 0 == rank ) printf( 
+    if( 0 == rank ) printf(
         "## Timings ########### Min         ### Max         ##\n"
         "Barrier (no fault)  # %13.5e # %13.5e\n"
         "Barrier (w/ fault)  # %13.5e # %13.5e\n"
-        , mtff, Mtff, mtwf, Mtwf );   
+        , mtff, Mtff, mtwf, Mtwf );
 }
 
